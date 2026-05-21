ed064b87300538520ced27706a10d71c964222ce
Mariusz Szczepańczyk <m@mariusz.sh>
2026-05-21 18:06:55 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		dts({
+			include: ["src"],
+			exclude: ["src/**/*.fixture.tsx", "src/**/*.test.tsx"],
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			formats: ["es"],
+			fileName: "index",
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				preserveModules: true,
+				preserveModulesRoot: "src",
+			},
+		},
+	},
 });
