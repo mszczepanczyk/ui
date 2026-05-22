11ae23068aab14317696361c292e53248cb00670
Mariusz Szczepańczyk <m@mariusz.sh>
2026-05-22 13:06:20 +0200

    WIP

--- src/cosmos.decorator.tsx
+++ src/cosmos.decorator.tsx
@@ -1,6 +1,25 @@
+import { MDXProvider } from "@mdx-js/react";
 import type { ReactNode } from "react";
-import "./index.css";
+
+// import "./index.css";
+
+// const _components = {
+// 	a: styled("a"),
+// 	blockquote: styled("blockquote"),
+// 	code: styled("code"),
+// 	h1: styled("h1"),
+// 	h2: styled("h2"),
+// 	h3: styled("h3"),
+// 	h4: styled("h4"),
+// 	h5: styled("h5"),
+// 	h6: styled("h6"),
+// 	li: styled("li"),
+// 	ol: styled("ol"),
+// 	p: styled("p"),
+// 	pre: styled("pre"),
+// 	ul: styled("ul"),
+// };
 
 export default function Decorator({ children }: { children: ReactNode }) {
-	return <>{children}</>;
+	return <MDXProvider>{children}</MDXProvider>;
 }
--- vite.cosmos.config.ts
+++ vite.cosmos.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "node:path";
+import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // Vite config specifically for Cosmos (no library mode)
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), mdx()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "./src"),
@@ -12,6 +13,9 @@ export default defineConfig({
 		},
 	},
 	root: __dirname,
+	css: {
+		devSourcemap: true,
+	},
 	build: {
 		rollupOptions: {
 			input: resolve(__dirname, "index.html"),
